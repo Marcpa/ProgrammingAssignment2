3db1a522d59eea94b069dc43d74408d84cb6c980
Marcello <marcpa@centroin.com.br>
2015-01-12 19:21:30 -0200

    coursera commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions can cache the inverse of a matrix
 
-## Write a short comment describing this function
-
+#This function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
+         i <- NULL 
+         # When the set function is called, Inverse is set to NULL and assigns the argument y to x 
+         set <- function(y){ 
+                 x <<- y
+                 i <<- NULL  
+         } 
+         get <- function() x # get function returns the matrix 
+          
+         setInverse <- function(solve) i <<- solve # setInverse overrides i and assigns the argument 
+          
+         getInverse <- function() i # getInverse returns the Inverse 
+        
+         # creates a list of the functions
+         list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)          
+ } 
 
-}
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+#This function computes the inverse of the special "matrix" returned by makeCacheMatrix function
+cacheSolve <- function(x, ...) { 
+         i <- x$getInverse() # Retrives the most recent value for the inverse 
+          
+         if(!is.null(i)){ 
+                 message("getting cached Matrix") 
+                 return(i) 
+                 # If the value of Inverse is NOT null, cacheSolve returns that value         
+         } 
+         # If Inverse is NULL, then retrieve matrix and calculate inverse with solve() function 
+         message("calculating Inverse Matrix") 
+         data <- x$get() 
+         i <- solve(data, ...) 
+         x$setInverse(i) # Sets Inverse to new value    
+         i ## Return a matrix that is the inverse of 'x'
+ } 
